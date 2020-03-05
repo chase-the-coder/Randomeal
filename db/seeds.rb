a7eca7e0c5aaaafeef58d66679d183fa1cec7f31
Lucas Cunha <lucasdinizcunha@uol.com.br>
2020-03-05 12:13:46 -0300

    created category seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+# city_grab = "https://developers.zomato.com/api/v2.1/cities?q=#{city}&apikey=a4f1d85868c5fa3598a0e5580a1a2215"
+
+# Category seeding
+
+Category.delete_all
+puts "Deleted all Categories"
+
+url = "https://developers.zomato.com/api/v2.1/cuisines?city_id=73&apikey=a4f1d85868c5fa3598a0e5580a1a2215"
+restaurant_api = open(url).read
+api = JSON.parse(restaurant_api)
+puts "Parsed Zomato API"
+
+api["cuisines"].each_with_index do |a|
+  Category.create!(name: a["cuisine"]["cuisine_name"])
+end
+puts "Category seeds created with success"
