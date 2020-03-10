b9853667387bf6dec716422d7f151f2a41c496c2
Pedro Arthur <pedro.art.vel@gmail.com>
2020-03-10 13:39:43 -0300

    Tried to add mapbox to restaurant index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,13 @@ class RestaurantsController < ApplicationController
   end
 
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.geocoded
+    @markers = @restaurants.map do |restaurant|
+      {
+        lat: restaurant.latitude,
+        lng: restaurant.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { restaurant: restaurant })
+      }
+    end
   end
 end
