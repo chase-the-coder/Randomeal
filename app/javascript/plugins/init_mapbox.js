70a636468b1aec5c54db20643097e2b7de749d89
Chase Lindsay <chase.lindsay1@gmail.com>
2020-03-10 14:54:45 -0300

    working on mapbox

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,5 @@
 import mapboxgl from 'mapbox-gl';
-
-
+const mapElement = document.getElementById('map');
 const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
@@ -8,24 +7,19 @@ const buildMap = () => {
     style: 'mapbox://styles/mapbox/streets-v10'
   });
 };
-
 const addMarkersToMap = (map, markers) => {
-  const popup = new mapboxgl.Popup().setHTML(markers.infoWindow);
+  // const popup = new mapboxgl.Popup().setHTML(markers.infoWindow);
   new mapboxgl.Marker()
     .setLngLat([ markers.lng, markers.lat ])
-    .setPopup(popup)
+    // .setPopup(popup)
     .addTo(map);
 };
-
-
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   bounds.extend([ markers.lng, markers.lat ]);
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
-
 const initMapbox = () => {
-  const mapElement = document.getElementById('map');
   if (mapElement) {
     const map = buildMap();
     const markers = JSON.parse(mapElement.dataset.markers);
@@ -33,5 +27,4 @@ const initMapbox = () => {
     fitMapToMarkers(map, markers);
   }
 };
-
 export { initMapbox };
